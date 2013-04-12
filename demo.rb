4c02b6036d525863c19335be4c9df6cd7c49619d
Tim Carey-Smith <tim@spork.in>
2013-04-12 13:06:29 +1200

    it works

--- demo.rb
+++ demo.rb
@@ -1,30 +1,68 @@
+require 'celluloid'
+
+class Child
+  include Celluloid
+
+  def initialize
+    @counter = 0
+  end
+  attr_reader :counter
+
+  def incr
+    @counter += 1
+  end
+
+  # reply with state
+  def finish
+    {
+      :counter => @counter,
+    }
+  end
+end
+
 class Router
   include Celluloid
-  
+
   def initialize
+    @cache = {}
     @children = {}
   end
-  
-  # on child exit, store the state
-  
-  def alive?(child)
-    if child.alive?
-      @children.fetch(child.mailbox).sync(:alive?)
+
+  trap_exit :on_exit
+
+  def counter(mailbox)
+    if @cache.key?(mailbox)
+      @cache.fetch(mailbox).fetch(:counter)
     else
-      @cache.fetch(child.mailbox).fetch(:alive?)
+      @children.fetch(mailbox).sync(:counter)
     end
   end
-  
-  def register(child)
+
+  def incr(mailbox)
+    @children.fetch(mailbox).sync(:incr)
+  end
+
+  def start_child
+    child = Child.new_link
     @children[child.mailbox] = child
     RouterProxy.new(current_actor, child)
+  end
+
+  def finish(mailbox)
+    data = @children.fetch(mailbox).sync(:finish)
+    @cache[mailbox] = data
+    @children.fetch(mailbox).terminate
+  end
+
+  def on_exit(actor, reason)
+    @children.delete(actor.mailbox)
   end
 end
 
 class RouterProxy < Celluloid::AbstractProxy
   def initialize(router, child)
-    @child = child
-    @proxy = SyncProxy.new(router.mailbox, router.class)
+    @child = child.mailbox
+    @proxy = ::Celluloid::SyncProxy.new(router.mailbox, router.class)
   end
 
   def method_missing(meth, *args, &block)
@@ -34,6 +72,12 @@ class RouterProxy < Celluloid::AbstractProxy
 end
 
 router = Router.new
-child = router.register(Child.new) # RouterProxy
+child = router.start_child
 
-child.alive? # router.alive?(Raw(child))
+p(actors: Celluloid::Actor.all.size)
+p(counter: child.counter)
+p(incr: child.incr)
+p(counter: child.counter)
+p(finish: child.finish)
+p(actors: Celluloid::Actor.all.size)
+p(counter: child.counter)
